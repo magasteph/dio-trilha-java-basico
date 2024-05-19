4e3c172a6b7859601642523a5bbc3ac5863dc2ae
magasteph <stephanne.victoria@gmail.com>
2024-05-18 22:12:00 -0300

    entendendo sobre os operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,12 +2,18 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         
         String meuNome = "Stephanne";
+        System.out.println(meuNome);
+
         
         double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
+
 
         int numero = 5;
         numero = 10;
