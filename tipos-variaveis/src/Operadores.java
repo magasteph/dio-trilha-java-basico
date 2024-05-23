5e92401f716293001251a3b3ed6d36e18d670f63
magasteph <stephanne.victoria@gmail.com>
2024-05-23 17:34:02 -0300

    exemplos de operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,5 +1,63 @@
 public class Operadores {
     public static void main(String[] args){
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+        }
+        System.out.println("Fim");
+
+        /* 
+
+        String nomeUm = "Stephanne";
+        String nomeDois = new String("Stephanne");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        System.out.println("NumeroUm é igual a numeroDois? " + simNao);
+        
+        simNao = numero1 != numero2;
+        System.out.println("NumeroUm é diferente a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+        System.out.println("NumeroUm é maior que o numeroDois? " + simNao);
+
+
+        int a, b;
+
+        a = 6;
+        b=6;
+        String resultado = a==b ? "verdadeiro" : "falso"; 
+        
+        System.out.println(resultado);
+
+
+        boolean variavel = false;
+        System.out.println(!variavel); //inversão de valor
+        System.out.println(variavel);
+        variavel = !variavel;
+        System.out.println(variavel);
+        
+        int numero = 5;
+        numero++;
+        System.out.println(numero++);
+        System.out.println(++numero);
+        System.out.println(numero--);
+        System.out.println(--numero);
+
+        int numero = 5;
+        numero = - numero;
+        System.out.println(numero);
+        
         String nomeCompleto = "LINGUAGEM " + "JAVA";
         System.out.println(nomeCompleto);
 
@@ -16,6 +74,6 @@ public class Operadores {
         concatenacao = "1" + (1+1+1);
         System.out.println(concatenacao);
 
-
+*/
     }
 }
