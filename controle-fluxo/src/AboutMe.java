1f15420524f650369151136c59112a208a741fc5
magasteph <stephanne.victoria@gmail.com>
2024-06-23 21:49:07 -0300

    try e catch

--- controle-fluxo/src/AboutMe.java
+++ controle-fluxo/src/AboutMe.java
@@ -1,6 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
-    
+    public static void main(String[] args) {
+        try{
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite a sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println(" Minha altura é " + altura + " cm ");
+        scanner.close();
+
+        }
+        catch(InputMismatchException e){
+            System.out.println("O campos idade e altura precisam ser numerico");
+        }
+    }
 }
