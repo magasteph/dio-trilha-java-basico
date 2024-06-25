19770cd6465d4c59271c787d857d4699e0e939e9
magasteph <stephanne.victoria@gmail.com>
2024-06-25 16:37:40 -0300

    metodo atender

--- controleCandidatos/src/candidatura/ProcessoSeletivo.java
+++ controleCandidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,42 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		imprimirSelecionados();
+		String[] candidatos = {"FELIPE","MARCIA","JULIA", "PAULO","AUGUSTO"};
+		for(String candidato : candidatos) {
+			entrandoEmContato(candidato);
+		}
 	}
+	
+	static void entrandoEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu = false;
+		
+		do {
+			atendeu = atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando) {
+				tentativasRealizadas++;
+			}else {
+				System.out.println("O contato foi realizado com sucesso");
+			}
+		}while(continuarTentando && tentativasRealizadas<3);
+		
+		if(atendeu) {
+			System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " Tentativa");
+		}else {
+			System.out.println("Não conseguimos contato com " + candidato + ", numero maximo de tentativas " + tentativasRealizadas + " realizadas" );
+		}
+	}
+	
+	static boolean atender() {
+		return new Random().nextInt(3)==1;
+	}
+	
+	
 	
 	static void imprimirSelecionados() {
 		String[] candidatos = {"FELIPE","MARCIA","JULIA", "PAULO","AUGUSTO"};
