a2bc14b450e974afa3678fa88e0762ae3025db08
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-07-23 20:42:55 -0300

    desafio

---
+++ Iphone/papeis/AparelhoTelefonico.java
@@ -0,0 +1,11 @@
+public class AparelhoTelefonico{
+    public void ligar(){
+        System.out.println("Ligando");
+    }
+    public void atender(){
+        System.out.println("Atendendo");
+    }
+    public void iniciarCorreioVoz(){
+        System.out.println("Iniciando Correio de Voz")
+    }
+}
