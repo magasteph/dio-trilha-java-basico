a9d47063edf17d2f463f6173978b84954ddcd2f3
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-07-23 18:49:46 -0300

    abstração

--- DesafioMSN/MSNMessenger.java
+++ DesafioMSN/MSNMessenger.java
@@ -1,4 +1,10 @@
 public class MSNMessenger extends ServicoMensagemInstanea{
-
+    public void enviarMensagem(){
+        System.out.println("Enviando mensagem pelo Messenger");
+    }
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem pelo Messenger");
+            
+    }
     
 }
--- DesafioMSN/ServicoMensagemInstanea.java
+++ DesafioMSN/ServicoMensagemInstanea.java
@@ -1,18 +1,4 @@
-public class ServicoMensagemInstanea{
-    
-    public void enviarMensagem(){
-        //encapsulamento
-        validarConectadoInternet();
-        System.out.println("Enviando Mensagem");
-        salvarHistoricoMensagem();
-    }
-    public void receberMensagem(){
-        System.out.println("Recebendo mensagem ");
-    }
-    private void validarConectadoInternet(){
-        System.out.println("Validando se está conectada a Internet");
-    }
-    private void salvarHistoricoMensagem(){
-        System.out.println("Salvando o historico da mensagem");
-    }
+public abstract class ServicoMensagemInstanea{
+    public abstract void enviarMensagem();
+    public abstract void receberMensagem();
 }
