793e0aa2cadb39711cd953e3dca8c6e23a587534
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-07-23 13:54:16 -0300

    iniciando

---
+++ Messenger/MSNMessenger.java
@@ -0,0 +1,14 @@
+public class MSNMessenger{
+    public void enviarMensagem(){
+        System.out.println("Enviando Mensagem");
+    }
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem");
+    }
+    public void validarConectadoInternet(){
+        System.out.println("Validando se está conectada a Internet");
+    }
+    public void salvarHistoricoMensagem(){
+        System.out.println("Salvando o historico da mensagem");
+    }
+}
