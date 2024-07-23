29550403725842bf9cc83038622ba2504d62fe33
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-07-23 18:41:15 -0300

    herança

--- DesafioMSN/MSNMessenger.java
+++ DesafioMSN/MSNMessenger.java
@@ -1,18 +1,4 @@
-public class MSNMessenger{
+public class MSNMessenger extends ServicoMensagemInstanea{
 
-    public void enviarMensagem(){
-        //encapsulamento
-        validarConectadoInternet();
-        System.out.println("Enviando Mensagem");
-        salvarHistoricoMensagem();
-    }
-    public void receberMensagem(){
-        System.out.println("Recebendo mensagem");
-    }
-    private void validarConectadoInternet(){
-        System.out.println("Validando se está conectada a Internet");
-    }
-    private void salvarHistoricoMensagem(){
-        System.out.println("Salvando o historico da mensagem");
-    }
+    
 }
---
+++ DesafioMSN/ServicoMensagemInstanea.java
@@ -0,0 +1,18 @@
+public class ServicoMensagemInstanea{
+    
+    public void enviarMensagem(){
+        //encapsulamento
+        validarConectadoInternet();
+        System.out.println("Enviando Mensagem");
+        salvarHistoricoMensagem();
+    }
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem ");
+    }
+    private void validarConectadoInternet(){
+        System.out.println("Validando se está conectada a Internet");
+    }
+    private void salvarHistoricoMensagem(){
+        System.out.println("Salvando o historico da mensagem");
+    }
+}
