5dc9d8d5ffb8f11d6bd960847817fdb2ad51ece1
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-07-23 18:26:04 -0300

    encapsulamento

--- Messenger/MSNMessenger.java
+++ Messenger/MSNMessenger.java
@@ -1,14 +1,18 @@
 public class MSNMessenger{
+
     public void enviarMensagem(){
+        //encapsulamento
+        validarConectadoInternet();
         System.out.println("Enviando Mensagem");
+        salvarHistoricoMensagem();
     }
     public void receberMensagem(){
         System.out.println("Recebendo mensagem");
     }
-    public void validarConectadoInternet(){
+    private void validarConectadoInternet(){
         System.out.println("Validando se está conectada a Internet");
     }
-    public void salvarHistoricoMensagem(){
+    private void salvarHistoricoMensagem(){
         System.out.println("Salvando o historico da mensagem");
     }
 }
