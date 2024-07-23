b57720af0d279b3ed74fcb0827e9b4f3db727b6e
VICTORIA MAGALHÃES FRANÇA <84507283+magasteph@users.noreply.github.com>
2024-07-23 12:13:08 -0300

    Encapsulamento

--- src/Autodromo.java
+++ src/Autodromo.java
@@ -2,5 +2,6 @@ public class Autodromo{
   public static void main(String[] args){
       Carro jeep = new Carro();
       jeep.ligar();
+
   }
 }
--- src/Carro.java
+++ src/Carro.java
@@ -1,5 +1,15 @@
 public class Carro {
   public void ligar(){
+    confereCambio();
+    confereCombustivel();
     System.out.println("Carro Ligado");
+  }
+  private void confereCombustivel(){
+    System.out.println("Conferindo combustivel");
+
+  }
+  private void confereCambio(){
+    System.out.println("Conferindo cambio em P");
+
   }
 }
